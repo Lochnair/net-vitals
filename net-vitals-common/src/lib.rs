7cd1119c08652ade576ae077b3d942f00cdc1805
Nils Andreas Svee <me@lochnair.net>
2026-03-04 00:53:00 +0700

    feat(common): Add ECN flags to FlowEvent

--- net-vitals-common/src/lib.rs
+++ net-vitals-common/src/lib.rs
@@ -3,10 +3,12 @@
 #[repr(C)]
 #[derive(Copy, Clone)]
 pub struct FlowEvent {
-    pub src_ip:   u32,
-    pub dst_ip:   u32,
+    pub src_ip: u32,
+    pub dst_ip: u32,
     pub src_port: u16,
     pub dst_port: u16,
+    pub ece: u16,
+    pub cwr: u16,
 }
 
 #[cfg(feature = "user")]
