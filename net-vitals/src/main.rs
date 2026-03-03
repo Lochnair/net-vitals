8714d46493ac63512508c44459e1da5a6f7f396f
Nils Andreas Svee <me@lochnair.net>
2026-03-03 23:25:34 +0700

    feat: Rework net-vitals binary to use library and `libc` for runtime

--- net-vitals/src/main.rs
+++ net-vitals/src/main.rs
@@ -1,13 +1,8 @@
 use std::net::Ipv4Addr;
+use std::sync::atomic::{AtomicBool, Ordering};
 
-use aya::maps::RingBuf;
-use aya::programs::{SchedClassifier, TcAttachType, tc};
 use clap::Parser;
-#[rustfmt::skip]
-use log::debug;
-use net_vitals_common::FlowEvent;
-use tokio::io::{Interest, unix::AsyncFd};
-use tokio::signal;
+use net_vitals::{FlowEvent, NetVitals};
 
 #[derive(Debug, Parser)]
 struct Opt {
@@ -15,70 +10,33 @@ struct Opt {
     iface: String,
 }
 
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    let opt = Opt::parse();
+static RUNNING: AtomicBool = AtomicBool::new(true);
 
-    env_logger::init();
+extern "C" fn handle_sigint(_: libc::c_int) {
+    RUNNING.store(false, Ordering::Relaxed);
+}
 
-    let rlim = libc::rlimit {
-        rlim_cur: libc::RLIM_INFINITY,
-        rlim_max: libc::RLIM_INFINITY,
+fn main() -> anyhow::Result<()> {
+    let Opt { iface } = Opt::parse();
+
+    unsafe {
+        libc::signal(
+            libc::SIGINT,
+            handle_sigint as *const () as libc::sighandler_t,
+        )
     };
-    let ret = unsafe { libc::setrlimit(libc::RLIMIT_MEMLOCK, &rlim) };
-    if ret != 0 {
-        debug!("remove limit on locked memory failed, ret is: {ret}");
-    }
 
-    let mut ebpf = aya::Ebpf::load(aya::include_bytes_aligned!(concat!(
-        env!("OUT_DIR"),
-        "/net-vitals"
-    )))?;
+    let mut nv = NetVitals::load(&iface)?;
+    println!("Listening on {iface}... (Ctrl-C to exit)");
 
-    let Opt { iface } = opt;
-    let _ = tc::qdisc_add_clsact(&iface);
+    nv.run(&RUNNING, |event: FlowEvent| {
+        let src = Ipv4Addr::from(u32::from_be(event.src_ip));
+        let dst = Ipv4Addr::from(u32::from_be(event.dst_ip));
+        let sp = u16::from_be(event.src_port);
+        let dp = u16::from_be(event.dst_port);
+        println!("New flow detected: {src}:{sp} -> {dst}:{dp}");
+    });
 
-    let program: &mut SchedClassifier = ebpf.program_mut("net_vitals").unwrap().try_into()?;
-    program.load()?;
-    program.attach(&iface, TcAttachType::Egress)?;
-
-    let program: &mut SchedClassifier =
-        ebpf.program_mut("net_vitals_ingress").unwrap().try_into()?;
-    program.load()?;
-    program.attach(&iface, TcAttachType::Ingress)?;
-
-    let ring_buf = RingBuf::try_from(ebpf.take_map("NEW_FLOWS").unwrap())?;
-    let mut async_fd = AsyncFd::with_interest(ring_buf, Interest::READABLE)?;
-
-    println!("Listening for new TCP connections... (Ctrl-C to exit)");
-
-    let ctrl_c = signal::ctrl_c();
-    tokio::pin!(ctrl_c);
-
-    loop {
-        tokio::select! {
-            _ = &mut ctrl_c => {
-                println!("Exiting...");
-                break;
-            }
-            guard = async_fd.readable_mut() => {
-                let mut guard = guard?;
-                let rb = guard.get_inner_mut();
-                while let Some(item) = rb.next() {
-                    // SAFETY: eBPF wrote a fully-initialized FlowEvent into this slot
-                    // via MaybeUninit::write; ring buffer guarantees 8-byte alignment
-                    // which satisfies FlowEvent's alignment of 4.
-                    let event = unsafe { &*(item.as_ptr() as *const FlowEvent) };
-                    let src = Ipv4Addr::from(u32::from_be(event.src_ip));
-                    let dst = Ipv4Addr::from(u32::from_be(event.dst_ip));
-                    let sp  = u16::from_be(event.src_port);
-                    let dp  = u16::from_be(event.dst_port);
-                    println!("New flow detected: {src}:{sp} -> {dst}:{dp}");
-                }
-                guard.clear_ready();
-            }
-        }
-    }
-
+    println!("Exiting...");
     Ok(())
 }
