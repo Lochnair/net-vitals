8194e1c3e8b1c5f1cd233b1a3bd04edf3d0fb667
Nils Andreas Svee <me@lochnair.net>
2026-03-05 13:29:10 +0700

    feat(cli): display RTT samples in the main output

--- net-vitals/src/main.rs
+++ net-vitals/src/main.rs
@@ -2,7 +2,7 @@ use std::net::Ipv4Addr;
 use std::sync::atomic::{AtomicBool, Ordering};
 
 use clap::Parser;
-use net_vitals::{FlowEvent, NetVitals};
+use net_vitals::{FlowEvent, NetVitals, RttSample};
 
 #[derive(Debug, Parser)]
 struct Opt {
@@ -29,16 +29,27 @@ fn main() -> anyhow::Result<()> {
     let mut nv = NetVitals::load(&iface)?;
     println!("Listening on {iface}... (Ctrl-C to exit)");
 
-    nv.run(&RUNNING, |event: FlowEvent| {
-        let src = Ipv4Addr::from(u32::from_be(event.src_ip));
-        let dst = Ipv4Addr::from(u32::from_be(event.dst_ip));
-        let sp = u16::from_be(event.src_port);
-        let dp = u16::from_be(event.dst_port);
-        println!(
-            "New flow detected: {src}:{sp} -> {dst}:{dp} [ECN: {0} CWR: {1}]",
-            event.ece, event.cwr
-        );
-    });
+    nv.run(
+        &RUNNING,
+        |event: FlowEvent| {
+            let src = Ipv4Addr::from(u32::from_be(event.src_ip));
+            let dst = Ipv4Addr::from(u32::from_be(event.dst_ip));
+            let sp = u16::from_be(event.src_port);
+            let dp = u16::from_be(event.dst_port);
+            println!(
+                "New flow: {src}:{sp} -> {dst}:{dp} [ECN: {0} CWR: {1}]",
+                event.ece, event.cwr
+            );
+        },
+        |sample: RttSample| {
+            let src = Ipv4Addr::from(u32::from_be(sample.src_ip));
+            let dst = Ipv4Addr::from(u32::from_be(sample.dst_ip));
+            let sp = u16::from_be(sample.src_port);
+            let dp = u16::from_be(sample.dst_port);
+            let rtt_us = sample.rtt_ns / 1_000;
+            println!("RTT sample: {src}:{sp} -> {dst}:{dp} rtt={rtt_us}µs");
+        },
+    );
 
     println!("Exiting...");
     Ok(())
